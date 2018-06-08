d8f9992d757f859d8ab8ad9dbdfe8c9cfcaa0404
Romain GALLIEN <r.gallien@santiane.fr>
2018-06-08 11:58:28 +0200

    Fix

--- tests/config/development/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-defined('UNIT_TEST_URL') OR define('UNIT_TEST_URL', 'https://local.dev/CodeIgniter/test_mock');
--- tests/config/testing/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-defined('UNIT_TEST_URL') OR define('UNIT_TEST_URL', 'http://localhost:80/test_mock');
