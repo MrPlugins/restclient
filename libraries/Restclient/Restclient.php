2f339749c842423ab245cc41962dd3ccec779415
Romain GALLIEN <r.gallien@santiane.fr>
2018-06-11 11:35:14 +0200

    Fix

--- libraries/Restclient/Restclient.php
+++ libraries/Restclient/Restclient.php
@@ -8,7 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Restclient
 {
-    const OUTPUT_TRUE = true;
+    /**
+     * Set default value of output
+     * @var boolean
+     */
+    const OUTPUT_DEBUG = false;
 
     /**
      * CI Instance
@@ -378,13 +382,14 @@ class Restclient
      * @param  boolean $output True will store the debug, False will print it
      * @return string
      */
-    public function debug($output = self::OUTPUT_TRUE)
+    public function debug($output = self::OUTPUT_DEBUG)
     {
         // Starting buffering content
         ob_start();
 
         // Set debug expand level to 10 by default
-        ref::config('expLvl', 10);
+        $dump = new ref();
+        $dump->config('expLvl', 10);
 
         // Load text helper to format code and add css to ouput
         echo '<style type="text/css">body{background-color:#fff;margin:40px;font:13px/20px normal Helvetica,Arial,sans-serif;color:#4F5155}a{color:#039;background-color:transparent;font-weight:400}h1{color:#444;background-color:transparent;border-bottom:1px solid #D0D0D0;font-size:19px;font-weight:400;margin:0 0 14px;padding:14px 15px 10px}code{font-family:Consolas,Monaco,Courier New,Courier,monospace;font-size:12px;background-color:#f9f9f9;border:1px solid #D0D0D0;color:#002166;display:block;margin:14px 0;padding:12px 10px}#body{margin:0 15px}p.footer{text-align:right;font-size:11px;border-top:1px solid #D0D0D0;line-height:32px;padding:0 10px;margin:20px 0 0}#container{margin:10px;border:1px solid #D0D0D0;box-shadow:0 0 8px #D0D0D0}</style>';
